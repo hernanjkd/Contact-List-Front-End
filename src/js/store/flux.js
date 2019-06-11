24d8e08665855f0aae64710980d90005e3d333a5
hernanjkd <hernanjkd@gmail.com>
2019-06-11 17:56:09 -0400

    working POST and GET

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,11 +1,58 @@
 const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
-			//Your data structures, A.K.A Entities
+			contactList: []
 		},
 		actions: {
-			//(Arrow) Functions that update the Store
-			// Remember to use the scope: scope.state.store & scope.setState()
+			addContact: (fullname, email, phone, address) => {
+				const url = "https://3000-cc2270b7-3663-47df-8934-859f16490208.ws-us0.gitpod.io/person";
+
+				fetch(url, {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify({
+						fullname: fullname,
+						email: email,
+						phone: phone,
+						address: address
+					})
+				});
+			},
+
+			getContact: () => {
+				const store = getStore();
+
+				const url = "https://3000-cc2270b7-3663-47df-8934-859f16490208.ws-us0.gitpod.io/person";
+
+				// fetch the url and get the response back
+				fetch(url, {
+					method: "GET",
+					headers: {
+						"Content-Type": "application/json"
+					}
+				})
+					// first must check if the fetch was able to reach the data successfully..
+					// if it was, turn it into JSON
+					.then(response => {
+						// one of the response properties that renders true if successful
+						if (response.status >= 400 && response.status !== 404) {
+							throw Error(Response.status + ": " + Response.statusText);
+						}
+						return response.json();
+					})
+
+					// use the fetched data that's in JSON format
+					.then(data => {
+						setStore({ contactList: data });
+					})
+
+					// If there was an error fetching the data, catch it here
+					.catch(error => {
+						console.log("Looks like there was a problem: \n", error);
+					});
+			}
 		}
 	};
 };
--- src/js/views/AddContact.js
+++ src/js/views/AddContact.js
@@ -1,7 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../store/appContext.js";
 
 export default class AddContact extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			status: null
+		};
+	}
+
 	render() {
 		return (
 			<div className="container">
@@ -10,27 +19,63 @@ export default class AddContact extends React.Component {
 					<form>
 						<div className="form-group">
 							<label>Full Name</label>
-							<input type="text" className="form-control" placeholder="Full Name" />
+							<input name="fullname" type="text" className="form-control" placeholder="Full Name" />
 						</div>
 						<div className="form-group">
 							<label>Email</label>
-							<input type="email" className="form-control" placeholder="Enter email" />
+							<input name="email" type="email" className="form-control" placeholder="Enter email" />
 						</div>
 						<div className="form-group">
 							<label>Phone</label>
-							<input type="phone" className="form-control" placeholder="Enter phone" />
+							<input name="phone" type="phone" className="form-control" placeholder="Enter phone" />
 						</div>
 						<div className="form-group">
 							<label>Address</label>
-							<input type="text" className="form-control" placeholder="Enter address" />
+							<input name="address" type="text" className="form-control" placeholder="Enter address" />
 						</div>
-						<button type="button" className="btn btn-primary form-control">
-							save
-						</button>
+						<Context.Consumer>
+							{({ store, actions }) => {
+								return (
+									<div>
+										<button
+											type="button"
+											className="btn btn-primary form-control"
+											onClick={() => {
+												let fullname = document.querySelector("[name=fullname]").value.trim();
+												let email = document.querySelector("[name=email]").value.trim();
+												let phone = document.querySelector("[name=phone]").value.trim();
+												let address = document.querySelector("[name=address]").value.trim();
+												if (phone === "") phone = null;
+												if (address === "") address = null;
+												actions.addContact(fullname, email, phone, address);
+												this.setState({ status: "Contact Sent" });
+												document.querySelector("form").reset();
+											}}>
+											POST
+										</button>
+										<button
+											type="button"
+											className="btn btn-secondary form-control"
+											onClick={() => {
+												actions.getContact();
+											}}>
+											GET
+										</button>
+									</div>
+								);
+							}}
+						</Context.Consumer>
 						<Link className="mt-3 w-100 text-center" to="/">
 							or get back to contacts
 						</Link>
 					</form>
+					{!this.state.status ? (
+						""
+					) : (
+						<div className="alert alert-success pt-3" role="alert">
+							{this.state.status}
+						</div>
+					)}
 				</div>
 			</div>
 		);
